e6d0edaedac7a53adeb747f07a5bbf0782413f72
Daniel Dada <doesdeos@gmail.com>
2023-09-11 00:32:39 +0300

    cleanup

--- yuki/scripts/devmon.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-devmon --no-gui --exec-on-drive "notify-send --icon=block-device --urgency=low \"Volume %l has been mounted\"" --exec-on-remove "notify-send --icon=block-device --urgency=low \"Volume %l has been removed\""
